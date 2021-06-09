de45d9c3154cd74f967ca0032d1e01ad2f07c90e
ismtkit <kit1590@icloud.com>
2021-06-09 20:59:35 +0000

    heroku用

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+   root to: 'homes#top'
+
+  scope module: 'user' do
+   devise_for :users, controllers: {
+      sessions: 'user/users/sessions',
+      registrations: 'user/users/registrations',
+      passwords: 'user/users/passwords'
+    }
+  end
+
+  namespace :admin do
+    devise_for :admins, controllers: {
+      sessions: 'client/admins/sessions',
+      registrations: 'client/admins/registrations',
+      passwords: 'client/admins/passwords'
+    }
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
