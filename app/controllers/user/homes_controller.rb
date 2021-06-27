2536d3fe014e79b22b739d679b036b469bb3876c
ismtkit <kit1590@icloud.com>
2021-06-27 18:54:26 +0000

    finish

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,8 +1,15 @@
 class User::HomesController < ApplicationController
   def top
-    @movieinfo_popular = JSON.parse((Tmdb::Movie.popular).to_json)
-    @movieinfo_top_rated = JSON.parse((Tmdb::Movie.top_rated).to_json)
-    @movieinfo_now_playing = JSON.parse((Tmdb::Movie.now_playing).to_json)
+      @type = params[:movie].present? ? params[:movie][:type] : 'popular'
+      if @type == 'popular'
+        @movies = JSON.parse((Tmdb::Movie.popular).to_json)
+      elsif @type == 'top_rated'
+        @movies = JSON.parse((Tmdb::Movie.top_rated).to_json)
+      elsif @type == 'now_playing'
+        @movies = JSON.parse((Tmdb::Movie.now_playing).to_json)
+      else
+        @movies = JSON.parse((Tmdb::Movie.popular).to_json)
+      end
   end
 
 end
--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,16 @@
 module HomesHelper
+
+  def movie_title(type)
+    case type
+    when 'popular'
+      '本日の世界のトレンド'
+    when 'top_rated'
+      '評価の高い映画'
+    when 'now_playing'
+      '上映中の映画'
+    else
+      '本日の世界のトレンド'
+    end
+  end
+
 end
